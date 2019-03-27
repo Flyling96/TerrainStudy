e97e7a86e7cdf293c44d6bff72f661e413077ed8
FlyLing <963452757@qq.com>
2019-03-27 11:48:13 +0800

    Unity地形分块导出Mesh、高度法线图

---
+++ Assets/Scripts/Editor/TerrainExport.cs
@@ -0,0 +1,557 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class TerrainExport : ScriptableWizard
+{
+
+    [MenuItem("Editor/ExportTerrainData")]
+    public static void OpenExportTerrainWnd()
+    {
+        ScriptableWizard.DisplayWizard<TerrainExport>("保存地形", "保存", "取消");
+    }
+
+    string mapName = "map004";
+    bool isChunk = true;
+    bool isSaveCPUMesh = true;
+    bool isSaveGPUMesh = true;
+    int chunkWidth = 300; //块宽度（单位米）
+    int chunkLength = 300; //块长度 （单位米）
+    int chunkQuadCountX = 300; //块中横向网格数
+    int chunkQuadCountZ = 300; //块中纵向网格数
+    protected override bool DrawWizardGUI()
+    {
+        mapName = EditorGUILayout.TextField("地形名称", mapName);
+        isChunk = EditorGUILayout.Toggle("是否分块", isChunk);
+        if (isChunk)
+        {
+            chunkWidth = EditorGUILayout.IntField("块宽度(X)", chunkWidth);
+            chunkLength = EditorGUILayout.IntField("块长度(Z)", chunkLength);
+            chunkQuadCountX = EditorGUILayout.IntField("块横向网格数(X)", chunkQuadCountX);
+            chunkQuadCountZ = EditorGUILayout.IntField("块纵向网格数(Z)", chunkQuadCountZ);
+        }
+        else
+        {
+            chunkQuadCountX = EditorGUILayout.IntField("横向网格数(X)", chunkQuadCountX);
+            chunkQuadCountZ = EditorGUILayout.IntField("纵向网格数(Z)", chunkQuadCountZ);
+        }
+
+        isSaveCPUMesh = EditorGUILayout.Toggle("保存CPU计算高度的Mesh", isSaveCPUMesh);
+        isSaveGPUMesh = EditorGUILayout.Toggle("保存GPU计算高度所需Mesh和材质", isSaveGPUMesh);
+
+        return true;
+    }
+
+    private void OnWizardUpdate()
+    {
+        errorString = "";
+        isValid = true;
+
+        if(Terrain.activeTerrain == null)
+        {
+            errorString = "当前场景没有地形";
+            isValid = false;
+            return;
+        }
+
+        if(string.IsNullOrEmpty(mapName))
+        {
+            errorString = "请输入要保存的地形名";
+            isValid = false;
+            return;
+        }
+
+        if(isChunk)
+        {
+            if(chunkWidth<0||chunkWidth > Terrain.activeTerrain.terrainData.size.x 
+               || chunkLength <0 || chunkLength > Terrain.activeTerrain.terrainData.size.z)
+            {
+                errorString = "块长宽不能为负数且不能大于地形长宽";
+                isValid = false;
+                return;
+            }
+        }
+
+
+    }
+
+    private void OnWizardCreate()
+    {
+        ExportTerrainData();
+    }
+
+    private void OnWizardOtherButton()
+    {
+        Close();
+    }
+
+
+    TerrainData data = null;
+    private float[,] heights = null;
+    private Vector3[,] normals = null;
+    private int heightmapWidth = 0; //高度图宽度（单位像素）
+    private int heightmapHeight = 0;  //高度图长度（单位像素）
+    int chunkCountX = 0;  //横向块数
+    int chunkCountZ = 0;  //纵向块数
+    int chunkPixelCountX = 0; //单块的对应高度图的像素数（横向）
+    int chunkPixelCountZ = 0; //单块的对应高度图的像素数（纵向）
+    float maxHeight = 0;
+    public void ExportTerrainData()
+    {
+        string path = Application.dataPath + "/TerrainData/";
+        string assetsPath = "Assets/TerrainData/";
+
+        data = Terrain.activeTerrain.terrainData;
+        path = path + mapName;
+        assetsPath = assetsPath + mapName;
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        float[,] tempHeights = data.GetHeights(0, 0, data.heightmapWidth, data.heightmapHeight);
+        heightmapWidth = data.heightmapHeight;
+        heightmapHeight = data.heightmapWidth;
+        heights = new float[heightmapWidth, heightmapHeight];
+        for (int i=0;i< heightmapHeight; i++)
+        {
+            for(int j=0;j< heightmapWidth; j++)
+            {
+                heights[i, j] = tempHeights[j, i];
+            }
+        }
+
+        normals = new Vector3[data.heightmapWidth, data.heightmapHeight];
+        maxHeight = data.size.y;
+
+
+        SaveHeightNormalMap(path);
+        if (isSaveCPUMesh)
+        {
+            SaveHeightMesh(assetsPath);
+            SaveTotalTerrainMaterial(assetsPath);
+        }
+
+        if (isChunk)
+        {
+            chunkCountX = (int)(data.size.x / chunkWidth) + (data.size.x % chunkWidth != 0 ? 1 : 0);
+            chunkCountZ = (int)(data.size.z / chunkLength) + (data.size.z % chunkLength != 0 ? 1 : 0);
+            chunkPixelCountX = (int)(heightmapWidth * chunkWidth / data.size.x) + 1;
+            chunkPixelCountZ = (int)(heightmapHeight * chunkLength / data.size.z) + 1;
+
+            string chunkPath = path + "/Chunks";
+            if (!Directory.Exists(chunkPath))
+            {
+                Directory.CreateDirectory(chunkPath);
+            }
+            SaveChunkHeightNormalMap(chunkPath);
+            if (isSaveGPUMesh)
+            {
+                SaveTerrainMesh(chunkWidth, chunkLength, assetsPath, true);
+            }
+            if (isSaveCPUMesh)
+            {
+                SaveChunkHeightMesh(assetsPath + "/Chunks");
+                SaveChunkTerrainMaterial(assetsPath + "/Chunks");
+            }
+        }
+        else
+        {
+            chunkCountX = 1;
+            chunkCountZ = 1;
+            chunkPixelCountX = heightmapWidth;
+            chunkPixelCountZ = heightmapHeight;
+        }
+
+        if (isSaveGPUMesh)
+        {
+            SaveTerrainMesh(data.size.x, data.size.z, assetsPath, false);
+        }
+
+    }
+
+    Vector2 EncodeHeight(float height)
+    {
+        Vector2 result;
+        if (height < 1)
+        {
+            result = new Vector2(1.0f, 255.0f) * height;
+            result = new Vector2(result.x , result.y - (int)result.y);
+            result.x -= result.y * 1.0f / 255.0f;
+        }
+        else
+        {
+            result.x = 1;
+            result.y = 0;
+        }
+        return result;
+    }
+
+    void SaveHeightNormalMap(string path)
+    {
+        EditorUtility.DisplayProgressBar("保存高度法线图", "保存高度法线图:0/1", 0);
+
+        string imageName = mapName + "_Total_HeightNormalMap.png";
+
+        Texture2D heightNormalMap = new Texture2D(data.heightmapWidth, data.heightmapHeight,TextureFormat.RGBA32, true);
+        Vector2 RG;
+
+        GetNormalInfo();
+
+        for (int j = 0; j < heightmapHeight; j++)
+        {
+            for (int i = 0; i < heightmapWidth; i++)
+            {
+                RG = EncodeHeight(heights[i, j]);
+                heightNormalMap.SetPixel(i, j, new Color(RG.x, RG.y, normals[i,j].x, normals[i,j].z));
+            }
+        }
+        heightNormalMap.Apply();
+
+        byte[] rawData = heightNormalMap.EncodeToPNG();
+        File.WriteAllBytes(path + "/" + imageName, rawData);
+        AssetDatabase.Refresh();
+
+        EditorUtility.ClearProgressBar();
+
+    }
+
+    void SaveChunkHeightNormalMap(string path)
+    {
+        string chunkPath = "";
+
+        for (int j = 0; j < chunkCountZ; j++)
+        {
+            for (int i = 0; i < chunkCountX; i++)
+            {
+                chunkPath = path + "/Chunk_" + (j * chunkCountX + i + 1);
+                EditorUtility.DisplayProgressBar("保存块高度法线图", "保存块高度法线图:" + (j * chunkCountX + i + 1)+"/"+ chunkCountX * chunkCountZ, (float)(j * chunkCountX + i + 1)/ (chunkCountX * chunkCountZ));
+                SaveSingleChunkHeightAndNormalMap(i, j, chunkPixelCountX, chunkPixelCountZ, j * chunkCountX + i + 1, chunkPath);
+            }
+        }
+
+        AssetDatabase.Refresh();
+        EditorUtility.ClearProgressBar();
+    }
+
+    void SaveSingleChunkHeightAndNormalMap(int countX,int countZ,int pixelCountX,int pixelCountZ,int sumCount,string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        string heightNormalName = mapName + "_Chunk_"+ sumCount + "_HeightNormalMap.png";
+
+        Texture2D heightNormalChunkMap = new Texture2D(pixelCountX , pixelCountZ,TextureFormat.RGBA32, true);
+        Vector2 RG;
+
+        for (int j = countZ * pixelCountZ; j < (countZ + 1) * pixelCountZ ; j++)
+        {
+            for (int i = countX * pixelCountX; i < (countX + 1) * pixelCountX ; i++)
+            {
+                if (i >= heightmapWidth || j >= heightmapHeight)
+                {
+                    int mini = Mathf.Min(i, heightmapWidth - 1);
+                    int minj = Mathf.Min(j, heightmapHeight - 1);
+                    RG = EncodeHeight(heights[mini, minj]);
+                    heightNormalChunkMap.SetPixel(i, j, new Color(RG.x, RG.y,normals[mini, minj].x, normals[mini, minj].z));
+                }
+                else
+                {
+                    RG = EncodeHeight(heights[i, j]);
+                    heightNormalChunkMap.SetPixel(i, j, new Color(RG.x, RG.y, normals[i, j].x, normals[i, j].z));
+                }
+            }
+        }
+
+        heightNormalChunkMap.Apply();
+
+        byte[] rawData = heightNormalChunkMap.EncodeToPNG();
+
+        File.WriteAllBytes(path + "/" + heightNormalName, rawData);
+
+        //保存材质
+        //if(isSaveGPUMesh)
+        //{
+        //    string materialPath = path.Replace(Application.dataPath, "Assets");
+        //    SaveTerrainMaterial(materialPath, mapName + "_Chunk_" + sumCount, heightNormalChunkMap);
+        //}
+
+    }
+
+    void GetNormalInfo()
+    {
+        float scaleX = data.size.x/ heightmapWidth;
+        float scaleZ = data.size.z / heightmapHeight;
+
+        float up, down, left, right;
+        Vector3 t, b, n;
+        for (int j = 0; j < heightmapHeight; j++)
+        {
+            for (int i = 0; i < heightmapWidth; i++)
+            {
+                up = j != heightmapHeight - 1?heights[i, j + 1]:heights[i,j];
+                down = j != 0 ? heights[i, j - 1] : heights[i, j];
+                right = i != heightmapWidth - 1 ? heights[i + 1, j] : heights[i, j];
+                left = i != 0? heights[i-1,j]: heights[i, j];
+                t = new Vector3(scaleX * 2, (right - left) * maxHeight, 0);
+                b = new Vector3(0, (up - down) * maxHeight, scaleZ * 2);
+                n = Vector3.Normalize(Vector3.Cross(b,t));
+                n = new Vector3(n.x * 0.5f + 0.5f, n.y * 0.5f + 0.5f, n.z * 0.5f + 0.5f);
+                normals[i, j] = n;
+            }
+        }
+
+    }
+
+    void SaveTerrainMesh(float width ,float length ,string path , bool isChunkMesh)
+    {
+        string meshName = "";
+        string tips = "";
+        if(isChunkMesh)
+        {
+            meshName = mapName + "_Chunk_Mesh.asset";
+            tips = "保存地形块Mesh";
+        }
+        else
+        {
+            meshName = mapName + "_Terrain_Mesh.asset";
+            tips = "保存地形Mesh";
+        }
+        int column = chunkQuadCountX;
+        int row = chunkQuadCountZ;
+
+        float spaceWidth = width / column;
+        float spaceLength = length / row;
+
+        int numberOfVertices = (row + 1) * (column + 1);
+        int numberOfIndex = row * column * 4;
+        Vector3[] vertices = new Vector3[numberOfVertices];
+        int[] indices = new int[numberOfIndex];
+        Vector2[] uvs = new Vector2[numberOfVertices];
+
+        for (int r = 0; r < row + 1; r++)
+        {
+            for (int c = 0; c < column + 1; c++)
+            {
+                vertices[r * (column + 1) + c] = new Vector3(c * spaceWidth, 0, r * spaceLength);
+                uvs[r * (column + 1) + c] = new Vector2((float)c / column, (float)r / row);
+            }
+        }
+
+        int index;
+        int line;
+        for (int r = 0; r < row; r++)
+        {
+            for (int c = 0; c < column; c++)
+            {
+                index = 4 * (r * column + c);
+                line = (column + 1);
+                indices[index] = (r + 1) * line + c;
+                indices[index + 1] = (r + 1) * line + c + 1;
+                indices[index + 2] = r * line + c + 1;
+                indices[index + 3] = r * line + c;
+            }
+        }
+
+        Mesh m_mesh = new Mesh();
+        //设置网格缓冲区为32位，默认为16位
+        m_mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        m_mesh.vertices = vertices;
+        m_mesh.SetIndices(indices, MeshTopology.Quads, 0);
+        List<Vector2> uvList = new List<Vector2>(uvs);
+        m_mesh.SetUVs(0, uvList);
+
+        EditorUtility.DisplayProgressBar(tips, tips+": 0/1", 0);
+
+        AssetDatabase.CreateAsset(m_mesh, path + "/" + meshName);
+
+        EditorUtility.ClearProgressBar();
+
+    }
+
+    void SaveChunkHeightMesh(string path)
+    {
+        string chunkPath = "";
+        for (int j = 0; j < chunkCountZ; j++)
+        {
+            for (int i = 0; i < chunkCountX; i++)
+            {
+                chunkPath = path + "/Chunk_" + (j * chunkCountX + i + 1);
+                EditorUtility.DisplayProgressBar("保存地形块高度Mesh", "保存地形块高度Mesh:" + (j * chunkCountX + i + 1) + "/" + chunkCountX * chunkCountZ, (float)(j * chunkCountX + i + 1) / (chunkCountX * chunkCountZ));
+                SaveSingleHeightMesh(true, chunkPath, i, j);
+            }
+        }
+    }
+
+    void SaveHeightMesh(string path)
+    {
+        EditorUtility.DisplayProgressBar("保存高度Mesh", "保存地形块高度Mesh: 0/1", 0);
+        SaveSingleHeightMesh(false, path);
+    }
+
+    void SaveSingleHeightMesh(bool isChunkMesh,string path,int countX = 0,int countZ = 0)
+    {
+        int width;
+        int length;
+        int column = chunkQuadCountX;
+        int row = chunkQuadCountZ;
+
+        string meshName;
+        int chunkCount = 0;
+
+        int tempChunkCountX = 1;
+        int tempChunkCountZ = 1;
+
+        if (isChunkMesh)
+        {
+            tempChunkCountX = chunkCountX;
+            tempChunkCountZ = chunkCountZ;
+            chunkCount = countZ * chunkCountX + countX + 1;
+            width = chunkWidth;
+            length = chunkLength;
+            meshName = mapName + "_Chunk_"+ chunkCount + "_CPUHeightMesh.asset";
+        }
+        else
+        {
+            width = (int)data.size.x;
+            length = (int)data.size.z;
+            meshName = mapName + "_Total_CPUHeightMesh.asset";
+        }
+
+        //之前的顶点数
+        int startVertexCountX = countX * chunkQuadCountX;
+        int startVertexCountZ = countZ * chunkQuadCountZ;
+
+        float spaceWidth = width / column;
+        float spaceLength = length / row;
+
+        int numberOfVertices = (row + 1) * (column + 1);
+        int numberOfIndex = row * column * 4;
+        Vector3[] vertices = new Vector3[numberOfVertices];
+        int[] indices = new int[numberOfIndex];
+        Vector2[] uvs = new Vector2[numberOfVertices];
+
+        int nowVertexCountX;
+        int nowVertexCountZ;
+        //高度图像素数和顶点数的比例
+        float PixelVertexProX = (float)heightmapWidth / (chunkQuadCountX * tempChunkCountX + 1);
+        float PixelVertexProZ = (float)heightmapHeight / (chunkQuadCountZ * tempChunkCountZ + 1);
+
+        int pixelX;
+        int pixelZ;
+
+        for (int r = 0; r < row + 1; r++)
+        {
+            nowVertexCountZ = startVertexCountZ + r + 1;
+            for (int c = 0; c < column + 1; c++)
+            {
+                nowVertexCountX = startVertexCountX + c + 1;
+                pixelX = (int)((nowVertexCountX - 1) * PixelVertexProX);
+                pixelZ = (int)((nowVertexCountZ - 1) * PixelVertexProZ);
+                vertices[r * (column + 1) + c] = new Vector3(c * spaceWidth, heights[pixelX,pixelZ] * maxHeight, r * spaceLength);
+                uvs[r * (column + 1) + c] = new Vector2((float)c / column, (float)r / row);
+            }
+        }
+
+        int index;
+        int line;
+        for (int r = 0; r < row; r++)
+        {
+            for (int c = 0; c < column; c++)
+            {
+                index = 4 * (r * column + c);
+                line = (column + 1);
+                indices[index] = (r + 1) * line + c;
+                indices[index + 1] = (r + 1) * line + c + 1;
+                indices[index + 2] = r * line + c + 1;
+                indices[index + 3] = r * line + c;
+            }
+        }
+
+        Mesh m_mesh = new Mesh();
+        m_mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        m_mesh.vertices = vertices;
+        m_mesh.SetIndices(indices, MeshTopology.Quads, 0);
+        List<Vector2> uvList = new List<Vector2>(uvs);
+        m_mesh.SetUVs(0, uvList);
+
+        AssetDatabase.CreateAsset(m_mesh, path + "/" + meshName);
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    void SaveTotalTerrainMaterial(string path)
+    {
+        EditorUtility.DisplayProgressBar("保存整体地形材质", "保存整体地形材质: 0/1", 0);
+
+        string totalPath = path + "/" + mapName + "_Total_HeightNormalMap.png";
+
+        TextureImporter heightNormalTexImporter = AssetImporter.GetAtPath(totalPath) as TextureImporter;
+        heightNormalTexImporter.wrapMode = TextureWrapMode.Clamp;
+        TextureImporterPlatformSettings heightNormalTexSetting = heightNormalTexImporter.GetDefaultPlatformTextureSettings();
+        heightNormalTexSetting.format = TextureImporterFormat.RGBA32;
+        heightNormalTexSetting.resizeAlgorithm = TextureResizeAlgorithm.Bilinear;
+        heightNormalTexImporter.SetPlatformTextureSettings(heightNormalTexSetting);
+        heightNormalTexImporter.SaveAndReimport();
+        Texture2D heightNormalTex = AssetDatabase.LoadAssetAtPath(totalPath, typeof(Texture2D)) as Texture2D;
+
+        SaveTerrainMaterial(path, mapName + "_Total_", heightNormalTex);
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    void SaveChunkTerrainMaterial(string path)
+    {
+        string chunkPath = "";
+        string matName = "";
+        string totalPath = "";
+        Texture2D heightNormalTex;
+        TextureImporter heightNormalTexImporter;
+        TextureImporterPlatformSettings heightNormalTexSetting;
+        for (int j = 0; j < chunkCountZ; j++)
+        {
+            for (int i = 0; i < chunkCountX; i++)
+            {
+                chunkPath = path + "/Chunk_" + (j * chunkCountX + i + 1);
+                matName = mapName + "_Chunk_" + (j * chunkCountX + i + 1);
+                totalPath = chunkPath + "/" + matName + "_HeightNormalMap.png";
+                heightNormalTexImporter = AssetImporter.GetAtPath(totalPath) as TextureImporter;
+                heightNormalTexImporter.wrapMode = TextureWrapMode.Clamp;
+                heightNormalTexSetting = heightNormalTexImporter.GetDefaultPlatformTextureSettings();
+                heightNormalTexSetting.format = TextureImporterFormat.RGBA32;
+                heightNormalTexSetting.resizeAlgorithm = TextureResizeAlgorithm.Bilinear;
+                heightNormalTexImporter.SetPlatformTextureSettings(heightNormalTexSetting);
+                heightNormalTexImporter.SaveAndReimport();
+                heightNormalTex = AssetDatabase.LoadAssetAtPath(totalPath, typeof(Texture2D)) as Texture2D;
+                EditorUtility.DisplayProgressBar("保存地形块地形材质", "保存地形块地形材质:" + (j * chunkCountX + i + 1) + "/" + chunkCountX * chunkCountZ, (float)(j * chunkCountX + i + 1) / (chunkCountX * chunkCountZ));
+                SaveTerrainMaterial(chunkPath, matName, heightNormalTex);
+            }
+        }
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    void SaveTerrainMaterial(string path,string name,Texture2D heightNormalTex)
+    {
+        string matPath = path + "/" +  name + "_mat.mat";
+        Material material = new Material(Shader.Find("Unlit/TerrainSimple"));
+        material.SetTexture("_HeightNormalTex", heightNormalTex);
+        material.SetFloat("_MaxHeight", data.size.y);
+        AssetDatabase.CreateAsset(material, matPath);
+    }
+
+
+    void SaveTotalPrefab(string path)
+    {
+        string prefabName = mapName + "_Total_Terrain.prefab";
+
+
+    }
+
+
+
+}
---
+++ Assets/Shader/TerrainSimple.shader
@@ -0,0 +1,62 @@
+﻿Shader "Unlit/TerrainSimple"
+{
+    Properties
+    {
+		_HeightNormalTex("Height Normal Texture",2D) = "white" {}
+		_MaxHeight("Max Height",Float) = 100.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _HeightNormalTex;
+			float _MaxHeight;
+
+			float DecodeHeight(float2 heightXY)
+			{
+				float2 decodeDot = float2(1.0f, 1.0f / 255.0f);
+				return dot(heightXY, decodeDot);
+			}
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+				float4 worldPos = mul(unity_ObjectToWorld, v.vertex);
+				o.uv = v.uv;
+				float2 heightTex = tex2Dlod(_HeightNormalTex, float4(o.uv.x, o.uv.y, 0, 0)).rg;
+				float height = DecodeHeight(heightTex);
+				worldPos.y = height;
+				o.vertex = mul(UNITY_MATRIX_VP, worldPos);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_HeightNormalTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
